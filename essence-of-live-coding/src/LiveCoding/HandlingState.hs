dc65f6ae6cf48cae9a105686f6d356843a947da0
Manuel Bärenz <programming@manuelbaerenz.de>
2021-06-01 12:32:39 +0200

    WIP custom monad

--- essence-of-live-coding/src/LiveCoding/HandlingState.hs
+++ essence-of-live-coding/src/LiveCoding/HandlingState.hs
@@ -10,6 +10,7 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NamedFieldPuns #-}
 module LiveCoding.HandlingState where
 
 -- base
@@ -97,6 +98,42 @@ data HandlingState m = HandlingState
   , destructors :: Destructors m
   }
   deriving Data
+
+instance Semigroup (HandlingState m) where
+  handlingState1 <> handlingState2 = HandlingState
+    { nHandles = nHandles handlingState1 `max` nHandles handlingState2
+    , destructors = destructors handlingState1 <> destructors handlingState2
+    }
+
+data MyHandlingState m a = MyHandlingState
+  { handlingState :: HandlingState m
+  , registered :: [Key]
+  , value :: a
+  }
+  deriving Functor
+
+newtype MyHandlingStateT m a = MyHandlingStateT
+  { unMyHandlingStateT :: m (MyHandlingState m a) }
+  deriving Functor
+
+instance Monad m => Monad (MyHandlingStateT m) where
+  return a = MyHandlingStateT $ return MyHandlingState
+    { handlingState = initHandlingState
+    , registered = []
+    , value = a
+    }
+  action >>= continuation = MyHandlingStateT $ do
+    firstState <- unMyHandlingStateT action
+    continuationState <- unMyHandlingStateT $ continuation $ value firstState
+    let registeredLater = registered continuationState
+        handlingStateEarlier = handlingState firstState <> handlingState continuationState
+        handlingStateLater = handlingStateEarlier
+          { destructors = destructors handlingStateEarlier `restrictKeys` IntSet.fromList registeredLater }
+    return MyHandlingState
+      { handlingState = handlingStateLater
+      , registered = registeredLater
+      , value = value continuationState
+      }
 
 initHandlingState :: HandlingState m
 initHandlingState = HandlingState
